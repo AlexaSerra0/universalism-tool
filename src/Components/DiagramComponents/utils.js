653b31afd738b26695b02ab8b8aab71d5ea91f46
AlexaSerra0 <a.lele@campus.fct.unl.pt>
2024-07-04 01:51:26 +0100

    diagram prettier

---
+++ src/Components/DiagramComponents/utils.js
@@ -0,0 +1,99 @@
+import { Position } from 'reactflow';
+
+// this helper function returns the intersection point
+// of the line between the center of the intersectionNode and the target node
+function getNodeIntersection(intersectionNode, targetNode) {
+  // https://math.stackexchange.com/questions/1724792/an-algorithm-for-finding-the-intersection-point-between-a-center-of-vision-and-a
+  const {
+    width: intersectionNodeWidth,
+    height: intersectionNodeHeight,
+    positionAbsolute: intersectionNodePosition,
+  } = intersectionNode;
+  const targetPosition = targetNode.positionAbsolute;
+
+  const w = intersectionNodeWidth / 2;
+  const h = intersectionNodeHeight / 2;
+
+  const x2 = intersectionNodePosition.x + w;
+  const y2 = intersectionNodePosition.y + h;
+  const x1 = targetPosition.x + targetNode.width / 2;
+  const y1 = targetPosition.y + targetNode.height / 2;
+
+  const xx1 = (x1 - x2) / (2 * w) - (y1 - y2) / (2 * h);
+  const yy1 = (x1 - x2) / (2 * w) + (y1 - y2) / (2 * h);
+  const a = 1 / (Math.abs(xx1) + Math.abs(yy1));
+  const xx3 = a * xx1;
+  const yy3 = a * yy1;
+  const x = w * (xx3 + yy3) + x2;
+  const y = h * (-xx3 + yy3) + y2;
+
+  return { x, y };
+}
+
+// returns the position (top,right,bottom or right) passed node compared to the intersection point
+function getEdgePosition(node, intersectionPoint) {
+  const n = { ...node.positionAbsolute, ...node };
+  const nx = Math.round(n.x);
+  const ny = Math.round(n.y);
+  const px = Math.round(intersectionPoint.x);
+  const py = Math.round(intersectionPoint.y);
+
+  if (px <= nx + 1) {
+    return Position.Left;
+  }
+  if (px >= nx + n.width - 1) {
+    return Position.Right;
+  }
+  if (py <= ny + 1) {
+    return Position.Top;
+  }
+  if (py >= n.y + n.height - 1) {
+    return Position.Bottom;
+  }
+
+  return Position.Top;
+}
+
+// returns the parameters (sx, sy, tx, ty, sourcePos, targetPos) you need to create an edge
+export function getEdgeParams(source, target) {
+  const sourceIntersectionPoint = getNodeIntersection(source, target);
+  const targetIntersectionPoint = getNodeIntersection(target, source);
+
+  const sourcePos = getEdgePosition(source, sourceIntersectionPoint);
+  const targetPos = getEdgePosition(target, targetIntersectionPoint);
+
+  return {
+    sx: sourceIntersectionPoint.x,
+    sy: sourceIntersectionPoint.y,
+    tx: targetIntersectionPoint.x,
+    ty: targetIntersectionPoint.y,
+    sourcePos,
+    targetPos,
+  };
+}
+
+export function createNodesAndEdges() {
+  const nodes = [];
+  const edges = [];
+  const center = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+
+  nodes.push({ id: 'Universalism', data: { label: 'Universalism' }, position: { x: 0, y: 0 } });
+  nodes.push({ id: 'Social Justice', data: { label: 'Social Justice' }, position: { x: 200, y: -60 } });
+  nodes.push({ id: 'Equality', data: { label: 'Equality' }, position: { x: -200, y: -60 } });
+  nodes.push({ id: 'Wisdom', data: { label: 'Wisdom' }, position: { x: 200, y: 150 } });
+  nodes.push({ id: 'Broadminded', data: { label: 'Broadminded' }, position: { x: -200, y: 150 } });
+  nodes.push({ id: 'Universalism Requirement', data: { label: 'Universalism Requirement' }, position: { x: 0, y: 250 } });
+  nodes.push({ id: 'Social Sustainability', data: { label: 'Social Sustainability' }, position: { x: 0, y: -150 } });
+
+  edges.push({ id: `uni-sj`, target: 'Universalism', source: `Social Justice`, type: 'floating',});
+  edges.push({ id: `uni-eq`, target: 'Universalism', source: `Equality`, type: 'floating',});
+  edges.push({ id: `uni-wis`, target: 'Universalism', source: `Wisdom`, type: 'floating',});
+  edges.push({ id: `uni-broa`, target: 'Universalism', source: `Broadminded`, type: 'floating',});
+  edges.push({ id: `uni-unireq`, target: 'Universalism', source: `Universalism Requirement`, type: 'floating',});
+  edges.push({ id: `uni-socialsus`, target: 'Universalism', source: `Social Sustainability`, type: 'floating',});
+  edges.push({ id: `sj-eq`, target: 'Social Justice', source: `Equality`, type: 'floating',});
+  edges.push({ id: `socialsus-eq`, target: 'Social Sustainability', source: `Equality`, type: 'floating',});
+
+
+  return { nodes, edges };
+}
--- src/Components/Tool.js
+++ src/Components/Tool.js
@@ -1,36 +1,31 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect  } from 'react';
 import "../App.css";
 import ReactFlow, {
   MiniMap,
   Controls,
+  addEdge,
   Background,
   useNodesState,
   useEdgesState,
+  useReactFlow,
+  ReactFlowProvider,
 } from 'reactflow';
 import 'reactflow/dist/style.css';
-import DiagramNode from './DiagramNode.js';
-import './DiagramNode.css';
+import DiagramNode from './DiagramComponents/DiagramNode.js';
+import './DiagramComponents/DiagramNode.css';
 
-const initialNodes = [
-  { id: 'Universalism', position: { x: 0, y: 0 }, data: { label: 'Universalism'}  },
-  { id: 'Social Justice', position: { x: 200, y: 0 }, data: { label: 'Social Justice'} },
-  { id: 'Equality', position: { x: -200, y: 0 }, data: { label: 'Equality'} },
-  { id: 'Wisdom', position: { x: -100, y: 100 }, data: { label: 'Wisdom'} },
-  { id: 'Broadminded', position: { x: 100, y: 100 }, data: { label: 'Broadminded' } },
-  { id: 'Universalism Requirement', position: { x: 0, y: 200 }, data: { label: 'Universalism Requirement' } },
-  { id: 'Social Sustainability', position: { x: 0, y: -200 }, data: { label: 'Social Sustainability' } },
-];
-const initialEdges = [
-  { id: 'uni-sj', source: 'Universalism', target: 'Social Justice'},
-  { id: 'uni-eq', source: 'Universalism', target: 'Equality'},
-  { id: 'uni-wis', source: 'Universalism', target: 'Wisdom'},
-  { id: 'uni-broad', source: 'Universalism', target: 'Broadminded'},
-  { id: 'uni-unireq', source: 'Universalism', target: 'Universalism Requirement'},
-  { id: 'uni-socials', source: 'Universalism', target: 'Social Sustainability'},
-  { id: 'eq-sj', source: 'Social Justice', target: 'Equality'},
-];
+import FloatingEdge from './DiagramComponents/FloatingEdge';
+import FloatingConnectionLine from './DiagramComponents/FloatingConnectionLine';
+import { createNodesAndEdges } from './DiagramComponents/utils';
+
+const { nodes: initialNodes, edges: initialEdges } = createNodesAndEdges();
+
 
 const nodeTypes = { diagramNode: DiagramNode };
+
+const edgeTypes = {
+  floating: FloatingEdge,
+};
 
 function Tool() {
   const [isOpen, setIsOpen] = useState(false);
@@ -41,18 +36,34 @@ function Tool() {
     setIsOpen(true);
   }, []);
 
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const { setCenter } = useReactFlow();
+
+  useEffect(() => {
+    setCenter(0, 0, { zoom: 1, duration: 800 });
+  }, [setCenter]);
 
   return (
     <div className='App'>
+      <div style={{paddingTop: '2rem'}}>
+        Horizontal process barrrrrr
+      </div>
+      <div style={{paddingTop: '2rem', paddingBottom: '2rem'}}>
+        Select barrrrrr
+      </div>
       <div className='diagramStyle'>
-        <div style={{ width: '90vw', height: '70vh', border: '5px solid #4BB4DE', borderRadius: '10px' }}>
+        <div className="floatingedges" style={{ width: '90vw', height: '80vh', border: '5px solid #4BB4DE', borderRadius: '10px' }}>
           <ReactFlow
             nodes={nodes}
             edges={edges}
             nodeTypes={nodeTypes}
+            edgeTypes={edgeTypes}
             onNodeClick={handleNodeClick}
+            //onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            connectionLineComponent={FloatingConnectionLine}
           >
             <Controls showInteractive={false} />
             <MiniMap />
@@ -67,4 +78,10 @@ function Tool() {
   );
 }
 
-export default Tool;
+export default function WrappedTool() {
+  return (
+    <ReactFlowProvider>
+      <Tool />
+    </ReactFlowProvider>
+  );
+}
